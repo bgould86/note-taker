851f820187406a1b3df84c678a5c5bc5e8322e1b
bgould86 <bgould86@gmail.com>
2022-02-28 16:55:29 -0600

    server successfully started

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,27 @@
+const notes = require("express").Router();
+
 //GET route for retrieving notes
+notes.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
 
 //POST route for new notes
+notes.post("/", (req, res) => {
+  console.log(req.body);
+
+  const { noteTitle, noteText } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      noteTitle,
+      noteText,
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error("Error in adding note");
+  }
+});
+
+module.exports = notes;
